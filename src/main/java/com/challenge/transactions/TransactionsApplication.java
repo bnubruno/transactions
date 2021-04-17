11403f227a46b0a81080a3ee824b61075e30ac19
Bruno Muehlbauer de  Souza <bruno.muehlbauer@contaazul.com>
2021-04-17 09:05:32 -0300

    first commit

---
+++ src/main/java/com/challenge/transactions/TransactionsApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.transactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/challenge/transactions/TransactionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.transactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
