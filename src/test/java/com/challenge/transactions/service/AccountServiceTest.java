5363fe227c75a56808ee5146d1bd29e8df170664
Bruno Muehlbauer de  Souza <bruno.muehlbauer@contaazul.com>
2021-04-18 16:00:24 -0300

    create account service test

--- src/test/java/com/challenge/transactions/service/AccountServiceTest.java
+++ src/test/java/com/challenge/transactions/service/AccountServiceTest.java
@@ -1,8 +1,12 @@
 package com.challenge.transactions.service;
 
+import static java.util.Optional.of;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
 import static org.mockito.Mockito.when;
+
+import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -33,7 +37,17 @@ public class AccountServiceTest {
 
 		assertThat( save.getId(), equalTo( 100L ) );
 		assertThat( save.getDocumentNumber(), equalTo( DOCUMENT_NUMBER ) );
+	}
 
+	@Test
+	public void whenFindById() {
+		when( repository.findById( 100L ) ).thenReturn( of( buildAccount( 100L ) ) );
+
+		Optional<Account> account = service.findById( 100L );
+
+		assertThat( account.isPresent(), is( true ) );
+		assertThat( account.get().getId(), equalTo( 100L ) );
+		assertThat( account.get().getDocumentNumber(), equalTo( DOCUMENT_NUMBER ) );
 	}
 
 	private Account buildAccount(Long accountId) {
